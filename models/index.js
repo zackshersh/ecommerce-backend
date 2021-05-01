349baf3fa5aca839ec8058de54d577c5db89b35d
Zack Hersh <zackshersh@gmail.com>
2021-05-01 15:08:49 -0400

    associations specified in index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsToMany(Tag, {
+  foreignKey: 'tag_id',
+})
 
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsToMany(Product, {
+  foreignKey: 'product_id',
+})
 
 module.exports = {
   Product,
