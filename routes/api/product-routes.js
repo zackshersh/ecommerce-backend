c0bb38a2819ca0b8c54491c334ad09f19e7896e2
Zack Hersh <zackshersh@gmail.com>
2021-05-01 15:15:59 -0400

    routes started

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,6 +6,12 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  try {
+    const productData = await Product.findAll()
+    res.status(200).json(productData)
+  } catch (err){
+    res.status(500).json(err)
+  }
   // be sure to include its associated Category and Tag data
 });
 
